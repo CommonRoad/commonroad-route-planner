6a82a6cb618f180aae0a246e615840b4efbf8307
Daniel Tar <tardani95@gmail.com>
2020-06-10 07:15:21 +0200

    additional error handling, fixed tabs in log messages

--- batch_processor.py
+++ batch_processor.py
@@ -73,6 +73,9 @@ scenarios_path_found = list()
 scenarios_path_not_found = list()
 scenarios_exception = list()
 
+scenarios_with_different_ids = list()
+scenarios_with_initial_state_error = list()
+
 plot_and_save_scenarios = True
 animate_scenario = False
 # Initialize Plotting
@@ -83,14 +86,19 @@ figsize = [60, 30]
 existing_scenario_ids_paths_dict = get_existing_scenarios(scenarios_root_folder)
 
 counter = 1
-for scenario, planning_problem_set in load_scenarios(scenarios_root_folder, scenario_ids):
+for idx, (scenario, planning_problem_set) in enumerate(load_scenarios(scenarios_root_folder, scenario_ids)):
     # retrieve planning problem with given index (for cooperative scenario:0, 1, 2, ..., otherwise: 0)
     # with the heuristic A* we do not want to solve cooperative scenarios so in all cases we will have
     # only one planning problem
     planning_problem_idx = 0
     planning_problem = list(planning_problem_set.planning_problem_dict.values())[planning_problem_idx]
 
-    scenario_id = scenario.benchmark_id
+    # scenario_id = scenario.benchmark_id
+    scenario_id = scenario_ids[idx]
+    if scenario_id != scenario.benchmark_id:
+        scenarios_with_different_ids.append(scenario_id)
+        logger.warning("Benchmark ID and scenario name differs in scenario: [{:<20}]".format(scenario_id))
+
     base_msg = "{:3}/{}\t[{:<20}]\t ".format(counter, num_of_scenarios_to_solve, scenario_id)
 
     routes = None
@@ -100,18 +108,18 @@ for scenario, planning_problem_set in load_scenarios(scenarios_root_folder, scen
     except IndexError as error:
         search_time = time.perf_counter() - time1
         scenarios_exception.append(scenario_id)
-        msg = base_msg + "\tsearch took\t{:10.4f}\tms\texception:".format(search_time * 1000)
+        msg = base_msg + "search took\t{:10.4f}\tms\texception:".format(search_time * 1000)
         logger.error(msg)
         logger.exception(error)
     except Exception as error:
         search_time = time.perf_counter() - time1
         scenarios_exception.append(scenario_id)
-        msg = base_msg + "\tsearch took\t{:10.4f}\tms\texception:".format(search_time * 1000)
+        msg = base_msg + "search took\t{:10.4f}\tms\texception:".format(search_time * 1000)
         logger.error(msg)
         logger.exception(error)
     else:
         search_time = time.perf_counter() - time1
-        msg = "\tsearch took\t{:10.4f}\tms\t".format(search_time * 1000)
+        msg = "search took\t{:10.4f}\tms\t".format(search_time * 1000)
 
         # test = dim(routes)
         if len(routes) > 1:
@@ -122,12 +130,12 @@ for scenario, planning_problem_set in load_scenarios(scenarios_root_folder, scen
 
         for ctn, route in enumerate(routes):
             if len(route) == 0:
-                logger.debug(msg + "path NOT FOUND")
+                logger.warning(msg + "path NOT FOUND")
                 scenarios_path_not_found.append(scenario_id)
                 continue
 
             goal_lanelet_id = route[-1]
-            logger.debug(msg + "path FOUND to goal lanelet: [{}]".format(goal_lanelet_id))
+            logger.debug(msg + "\tpath FOUND to goal lanelet: [{}]".format(goal_lanelet_id))
             scenarios_path_found.append(scenario_id)
             if plot_and_save_scenarios:
 
@@ -144,7 +152,11 @@ for scenario, planning_problem_set in load_scenarios(scenarios_root_folder, scen
                 fig.gca().autoscale()
 
                 # draw ego vehicle - with a collision object - uses commonroad_cc.visualizer
-                draw_initial_state(planning_problem, fig_num)
+                try:
+                    draw_initial_state(planning_problem, fig_num)
+                except AssertionError as error:
+                    scenarios_with_initial_state_error.append(scenario_id)
+                    logger.exception(error)
 
                 for route_lanelet_id in route:
                     lanelet = scenario.lanelet_network.find_lanelet_by_id(route_lanelet_id)
@@ -208,6 +220,8 @@ for scenario, planning_problem_set in load_scenarios(scenarios_root_folder, scen
 
     counter += 1
 
+logger.info("="*30+" RESULTS "+"="*30)
+
 logger.debug("Successfully SOLVED scenarios {:3}/{}:".format(len(scenarios_path_found), num_of_scenarios_to_solve))
 for scen in scenarios_path_found:
     logger.debug("\t{}".format(scen))
@@ -215,9 +229,20 @@ logger.warning(
     "Scenarios with path NOT FOUND {:3}/{}:".format(len(scenarios_path_not_found), num_of_scenarios_to_solve))
 for scen in scenarios_path_not_found:
     logger.warning("\t{}".format(scen))
-logger.critical("Scenarios with EXCEPTION {:3}/{}:".format(len(scenarios_exception), num_of_scenarios_to_solve))
+logger.critical("Scenarios with EXCEPTION in path finding {:3}/{}:".format(len(scenarios_exception), num_of_scenarios_to_solve))
 for scen in scenarios_exception:
     logger.critical("\t{}".format(scen))
+
+logger.info("="*30+" OTHER WARNINGS "+"="*30)
+logger.warning("Scenarios with different name and benchmark ID {:3}/{}:".format(len(scenarios_with_different_ids),
+                                                                                num_of_scenarios_to_solve))
+for scen in scenarios_with_different_ids:
+    logger.warning("\t{}".format(scen))
+
+logger.warning("Scenarios with initial state assertion {:3}/{}:".format(len(scenarios_with_initial_state_error),
+                                                                        num_of_scenarios_to_solve))
+for scen in scenarios_with_initial_state_error:
+    logger.warning("\t{}".format(scen))
 
 # switch of interactive mode not to disappear the figures at the end of the program
 mpl.rc_context(rc={'interactive': False})
