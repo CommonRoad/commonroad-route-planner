Weighting fix

commonroad_route_planner/route_planner.py
@@ -301,12 +301,12 @@ class RoutePlanner:
             # add edge if left lanelet
             adj_left = lanelet.adj_left
             if adj_left and lanelet.adj_left_same_direction and adj_left in self.allowed_lanelet_ids:
-                edges.append((lanelet.lanelet_id, lanelet.adj_left, {'weight': np.inf}))
+                edges.append((lanelet.lanelet_id, lanelet.adj_left, {'weight': 1.0}))
 
             # add edge if right lanelet
             adj_right = lanelet.adj_right
             if adj_right and lanelet.adj_right_same_direction and adj_right in self.allowed_lanelet_ids:
-                edges.append((lanelet.lanelet_id, lanelet.adj_right, {'weight': np.inf}))
+                edges.append((lanelet.lanelet_id, lanelet.adj_right, {'weight': 1.0}))
 
         # add all nodes and edges to graph
         graph.add_nodes_from(nodes)