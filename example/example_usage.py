9c05c5972517879fb9e128be5717f86d19f25e15
peterkocsis <peter.kocsis@tum.de>
2020-07-14 10:03:30 +0200

    Survival navigator

--- example/example_usage.py
+++ example/example_usage.py
@@ -7,7 +7,7 @@ from commonroad_route_planner.route_planner import RoutePlanner
 
 
 if __name__ == "__main__":
-    scenario_path = 'example_scenarios/USA_US101-32_1_T-1.xml'
+    scenario_path = 'example_scenarios/DEU_Muc-1_2_T-1.xml'
 
     print(f"Loading scenario {scenario_path}")
 
