50c852f4c04e450936505e6404d4f4c6b6c5f39c
Gerald Würsching <gerald.wuersching@tum.de>
2023-06-17 10:34:36 +0200

    minor fix route.py

--- commonroad_route_planner/route.py
+++ commonroad_route_planner/route.py
@@ -60,7 +60,7 @@ class Route:
         self.path_orientation = self._compute_orientation_from_polyline(self.reference_path)
         self.path_curvature = self._compute_curvature_from_polyline(self.reference_path)
 
-        if 'pycrccosy' in sys.modules:
+        if 'commonroad_dc.pycrccosy' in sys.modules:
             # make sure the reference path is already resampled and smoothened before creating a CLCS out of it
             self.CLCS = pycrccosy.CurvilinearCoordinateSystem(self.reference_path)
 
