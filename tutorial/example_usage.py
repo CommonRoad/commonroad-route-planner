2bb0ba8237fa38a217cbb7a2a5771b767889f613
Luis Gressenbuch <luis.gressenbuch@tum.de>
2023-09-14 16:33:29 +0200

    Fix path

--- tutorial/example_usage.py
+++ tutorial/example_usage.py
@@ -1,22 +1,31 @@
 import os
+from pathlib import Path
 
 from commonroad.common.file_reader import CommonRoadFileReader
 
 from commonroad_route_planner.route_planner import RoutePlanner
 from commonroad_route_planner.utility.visualization import visualize_route
 
+
 def main():
     # ========== initialization =========== #
-    path_scenarios = os.path.join(os.getcwd(), "../scenarios/")
-    id_scenario = 'USA_US101-22_1_T-1'
+    path_scenarios = Path(__file__).parents[1] / "scenarios"
+    id_scenario = "USA_US101-22_1_T-1"
     # read in scenario and planning problem set
-    scenario, planning_problem_set = CommonRoadFileReader(f"{path_scenarios}{id_scenario}.xml").open()
+    scenario, planning_problem_set = CommonRoadFileReader(
+        f"{path_scenarios / id_scenario}.xml"
+    ).open()
     # retrieve the first planning problem in the problem set
     planning_problem = list(planning_problem_set.planning_problem_dict.values())[0]
 
     # ========== route planning =========== #
     # instantiate a route planner with the scenario and the planning problem
-    route_planner = RoutePlanner(scenario, planning_problem, backend=RoutePlanner.Backend.NETWORKX_REVERSED, reach_goal_state=False)
+    route_planner = RoutePlanner(
+        scenario,
+        planning_problem,
+        backend=RoutePlanner.Backend.NETWORKX_REVERSED,
+        reach_goal_state=False,
+    )
     # plan routes, and save the routes in a route candidate holder
     candidate_holder = route_planner.plan_routes()
 
@@ -32,6 +41,7 @@ def main():
 
     # ========== visualization =========== #
     visualize_route(route, draw_route_lanelets=True, draw_reference_path=True)
+
 
 if __name__ == "__main__":
     main()
